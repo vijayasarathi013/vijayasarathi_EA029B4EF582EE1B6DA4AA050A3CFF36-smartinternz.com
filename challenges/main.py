0eb344f8ddadf4edeb289ff3665374df97faa2ea
vijayasarathi013 <144545509+vijayasarathi013@users.noreply.github.com>
2023-09-09 20:55:59 +0530

    Add files via upload

---
+++ challenges/main.py
@@ -0,0 +1,11 @@
+#python 3 program to find
+#factorial of given number
+def factorial (n): 
+
+  if (n==0 or n==1):
+    return 1
+  else:
+    return n * factorial(n - 1)
+num = 5
+r=factorial(num) 
+print("factorial of {} is {}.".format(num,r))
